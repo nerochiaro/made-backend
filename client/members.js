15f64f43db7eac9bd6f91ae4e21c88b3ae56ed40
Ugo Riboni <ugo.riboni@canonical.com>
2015-01-28 23:46:43 +0100

    Comment complicated function for posterity

--- client/members.js
+++ client/members.js
@@ -53,22 +53,35 @@ Template.memberpicker.helpers({
 
 Template.memberPayments.helpers({
   months: function () {
+    // get all membership-related payments belonging to
+    // this member, for which a period has been specified,
+    // and return them in chonological order.
     var p = Payments.find({$and: [
+      {$or: [{type: 'Membership'}, {type: 'Member+Laser'}]},
       {member: this._id},
-      {type: 'Membership'},
       {months: {$exists: 1}}
       ]}, {fields: {months: 1}}).fetch();
+
     if (p.length == 0) return [];
-    var cal = [];
+
+    // extract all periods and remove duplicates from all of them,
+    // then convert them to moments so we can work on them easily
     p = _.sortBy(_.flatten(_.pluck(p, 'months')), _.identity);
-    if (this._id == 7) console.log(p);
-    p.map(function(m) { return moment(m, 'YYYYMM') }).forEach(function(m) {
-      if (cal.length == 0) cal.push(m)
+    p = p.map(function(m) { return moment(m, 'YYYYMM')});
+
+    // build the calendar for this user by adding months that
+    // are present as-is, filling in the gaps with missing months,
+    // and marking duplicate months as such.
+    var cal = [];
+    p.forEach(function(m) {
+      if (cal.length == 0) cal.push(m);
       else {
+        // order is chronological, so this will spot all duplicates
         var last = _.last(cal);
         if (last.isSame(m, 'month')) last.isDuplicate = true;
         else {
-          // fill in missing months if there are gaps
+          // if this month is more than one month away from the last
+          // we have a gap that we need to fill
           var d = m.diff(last, 'month');
           if (d > 1) {
             for (var i = 1; i < d; i++) {
